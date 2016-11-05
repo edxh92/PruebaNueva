adc451c1681562df073648f4630b61fbf1ff86dd
edxh <edxh@EDXH92>
2016-11-05 09:54:17 -0600

    SolucionCompleta

---
+++ Prueba/UI/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UI.Startup))]
+namespace UI
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
